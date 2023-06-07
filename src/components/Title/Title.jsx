9b6aaa741fbee888c00a152b469bbccb661b9028
SandeepJoshi <joshisandip111@gmail.com>
2023-06-07 21:34:20 +0545

    added social media

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-
+import React from "react";
+import "./title.css";
+import { AiFillInstagram } from "react-icons/ai";
 const Title = () => {
   return (
-    <div className='title'>
-        <h1>3D Workshop</h1>
-        <h2>Gallery</h2>
-        <p>Lorem ipsum, dolor sit amet consectetur ad</p>
+    <div className="title">
+      <h1>3D Workshop</h1>
+      <a href="https://www.instagram.com/3dwork_shop/">
+        <AiFillInstagram />
+      </a>
+      <h2>Gallery</h2>
+      <p>
+        This is a website to showcase all the 3D model I created using blender
+        app
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Title
+export default Title;
